997aec260f7eed6c91486a20d1636685d510963b
sunyang <sun.yang@husky.neu.edu>
2018-07-28 12:41:54 -0700

    add backend authentication

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://Sophia:sophia123@ds031978.mlab.com:31978/sophiadevconnector"
+    "mongodb://Sophia:sophia123@ds031978.mlab.com:31978/sophiadevconnector",
+  secretOrKey: "secret"
 };
