cbd4c79e50856dbd67c57d21f4ced0924f7d51d9
sunyang <sun.yang@husky.neu.edu>
2018-08-03 13:42:30 -0700

    hide config keys

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI:
-    "mongodb://Sophia:sophia123@ds031978.mlab.com:31978/sophiadevconnector",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -114,14 +114,15 @@ router.post(
     //Get fields
     const profileFields = {};
     profileFields.user = req.user.id;
-    if (req.body.handle) profileFields.handle = req.body.handle;
-    if (req.body.company) profileFields.company = req.body.company;
-    if (req.body.website) profileFields.website = req.body.website;
-    if (req.body.location) profileFields.location = req.body.location;
-    if (req.body.bio) profileFields.bio = req.body.bio;
-    if (req.body.status) profileFields.status = req.body.status;
-    if (req.body.githubusername)
-      profileFields.githubusername = req.body.githubusername;
+    profileFields.handle = req.body.handle ? req.body.handle : "";
+    profileFields.company = req.body.company ? req.body.company : "";
+    profileFields.website = req.body.website ? req.body.website : "";
+    profileFields.location = req.body.location ? req.body.location : "";
+    profileFields.bio = req.body.bio ? req.body.bio : "";
+    profileFields.status = req.body.status ? req.body.status : "";
+    profileFields.githubusername = req.body.githubusername
+      ? req.body.githubusername
+      : "";
     //skills - split into array
     if (typeof req.body.skills !== "undefined") {
       profileFields.skills = req.body.skills.split(",");
